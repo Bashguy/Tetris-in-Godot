0c54affe420fb6e4faae9a09ecda791d5a8b3225
Bashguy <ssjsameer9000@gmail.com>
2024-09-15 21:31:56 -0400

    Added in Movement

--- TileMap.gd
+++ TileMap.gd
@@ -43,26 +43,112 @@ var j180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(2, 2)]
 var j270 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(0, 2), Vector2i(1, 2)]
 var j := [j0, j90, j180, j270]
 
-var tetrs := [i, t, o, z, s, l, j]
+#Array to hold the different shapes as well as a duplicate array to select a true random tetr
+var shapes := [i, t, o, z, s, l, j]
+var shapesFull := shapes.duplicate()
 
 #Constants for grid
 const COLUMNS : int = 10
 const ROWS : int = 20
+
+#Variables for movement
+const directions := [Vector2i.LEFT, Vector2i.RIGHT, Vector2i.DOWN]
+var steps : Array
+const requiredSteps : int = 50
+const startPosition := Vector2i(5, 1)
+var currentPosition : Vector2i
+var speed : float
 
 #Variables for the different tetrs
 var tetrShape
 var nextTetrShape
 var rotateIndex : int = 0
 var currTetr : Array
+
+#Variables for the tilemap
+var tileId :int = 0
+var tetrAtlas : Vector2i
+var nextTetrAtlas : Vector2i
+
+#Variables for the player
+var boardLayer : int = 0
+var currLayer : int = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	newGame()
+	
+func newGame():
+	speed = 1.0
+	#Index 0 is left 1 is right and 2 is down
+	steps = [0, 0, 0]
+	tetrShape = selectTetr()
+	#Assigns our atlas variable to set colors for each unique shape for the tetrs
+	tetrAtlas = Vector2i(shapesFull.find(tetrShape), 0)
+	newTetr()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("ui_left"):
+		steps[0] += 10
+	elif Input.is_action_pressed("ui_right"):
+		steps[1] += 10
+	elif Input.is_action_pressed("ui_down"):
+		steps[2] += 10
+	#Downward movement for each frame
+	steps[2] += speed
+	#Moving the tetr
+	for i in range(steps.size()):
+		if steps[i] > requiredSteps:
+			moveTetr(directions[i])
+			steps[i] = 0
 
-func createPiece(piece, pos, atlas):
+#Function to select a random tetr
+func selectTetr():
+	var tetr
+	#If the shapes array is not empty it will shuffle the shapes and remove the front tetr 
+	if not shapes.is_empty():
+		shapes.shuffle()
+		tetr = shapes.pop_front()
+	#If the array is empty fill the array with the duplicate and select another random tetr
+	else:
+		shapes = shapesFull.duplicate()
+		shapes.shuffle()
+		tetr = shapes.pop_front()
+	return tetr
+
+#Function to reset our variables and to create a new tetr
+func newTetr():
+	steps = [0, 0, 0]
+	currentPosition = startPosition
+	currTetr = tetrShape[rotateIndex]
+	createTetr(currTetr, currentPosition, tetrAtlas)
+
+#Function to remove any trail made by moving a piece
+func removeTetr():
+	for i in currTetr:
+		erase_cell(currLayer, currentPosition + i)
+
+#Function to create a piece on the board
+func createTetr(piece, pos, atlas):
 	for i in piece:
-	  set_cell()
+		set_cell(currLayer, pos + i, tileId, atlas)
+
+#Function to move the tetr in a direction
+func moveTetr(dir):
+	if moveCheck(dir):
+		removeTetr()
+		currentPosition += dir
+		createTetr(currTetr, currentPosition, tetrAtlas)
+
+#Function that checks if there is space to move
+func moveCheck(dir):
+	var mc = true
+	for i in currTetr:
+		if not isFree(i + currentPosition + dir):
+			mc = false
+	return mc
+
+#Function that returns the id for the tileset thats used in a cell if not empty, if it is it returns -1
+func isFree(pos):
+	return get_cell_source_id(boardLayer, pos) == -1
